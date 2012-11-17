66b893e8c513eaf3acd20861b3c8217de98c0125
alice <takemehighermore@gmail.com>
2012-11-17 11:39:33 +0900

    fatal bug fixed. changed word 'commandhash' to 'commithash'

--- setup.py
+++ setup.py
@@ -5,31 +5,32 @@ import os
 from distutils.core import setup
 
 class Information(object):
-    version="1.0.0"
-    author="alice1017"
-    author_github="http://github.com/alice1017"
-    author_twitter="http://twitter.com/alice1017"
-    author_email="takemehighermore@gmail.com"
-    author_blog="http://intention.hateblo.jp"
-    license="MIT"
-    description="The command line tools about github."
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst'),"r").read().replace(".. sourcecode:: shellscript","::")
+    version = "1.0.0"
+    author = "alice1017"
+    author_github = "http://github.com/alice1017"
+    author_twitter = "http://twitter.com/alice1017"
+    author_email = "takemehighermore@gmail.com"
+    author_blog = "http://intention.hateblo.jp"
+    license = "MIT"
+    description = "The command line tools about github."
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README.rst'),"r").read().replace(".. sourcecode:: shellscript","::")
 
-info = Information()
+info  =  Information()
 
 # build tools
-os.system("cp ./commandhash.py bin/commandhash;chmod +x ./bin/commandhash")
+os.system("cp ./commithash.py bin/commithash;chmod +x ./bin/commithash")
 
 setup(
-    name="gitTools",
-    author=info.author,
-    author_email=info.author_email,
-    version=info.version,
-    license=info.license,
-    url=info.author_github,
-    download_url=info.author_github+"/gitTools",
-    description=info.description,
-    long_description=info.long_description,
-    py_modules=['util.core', 'util.color'],
-    scripts=['bin/commithash']
+    name = "gitTools",
+    author = info.author,
+    author_email = info.author_email,
+    version = info.version,
+    license = info.license,
+    url = info.author_github,
+    download_url = info.author_github+"/gitTools",
+    description = info.description,
+    long_description = info.long_description,
+    py_modules = ['util.core', 'util.color'],
+    scripts = ['bin/commithash'],
+    required = ['termcolor', 'miniparser']
 )
