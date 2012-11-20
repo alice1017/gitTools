889847e9c4edd2e1f3d0f258dd2587e8ffae2437
alice <takemehighermore@gmail.com>
2012-11-21 01:13:10 +0900

    pep8準拠

--- util/color.py
+++ util/color.py
@@ -4,24 +4,31 @@
 import termcolor
 
 #blue, grey, yellow, green, cyan, magenta, white, red
+
+
 def blue(string):
     return termcolor.colored(string, "blue")
+
 
 def grey(string):
     return termcolor.colored(string, "grey")
 
+
 def yellow(string):
     return termcolor.colored(string, "yellow")
+
 
 def green(string):
     return termcolor.colored(string, "green")
 
+
 def cyan(string):
     return termcolor.colored(string, "cyan")
+
 
 def magenta(string):
     return termcolor.colored(string, "magenta")
 
+
 def red(string):
     return termcolor.colored(string, "red")
-
