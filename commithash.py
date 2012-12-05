d3e282da45df57c98285361fef25ae54b8d23fec
alice <takemehighermore@gmail.com>
2012-12-05 10:51:55 +0900

    Change parse_commit function specify.

--- commithash.py
+++ commithash.py
@@ -4,7 +4,10 @@
 import os
 import commands
 import miniparser
-import util.core as core
+
+from util import core
+from util import adjust
+from util.git import *
 from util.color import *
 
 parser = miniparser.parser(
@@ -12,14 +15,14 @@ parser = miniparser.parser(
 
 @parser.default(description="You can show latest commit hash.")
 def latest():
-    commits = core.get_commits()
+    commits = adjust.get_commits()
     print commits[0].commithash
 
 @parser.option(
     "ls", 
     description="You can show all commit hash with date, comment")
 def show_all():
-    commits = core.get_commits()
+    commits = adjust.get_commits()
     width = core.terminal_width()
     author_length = list(sorted([len(i.author.name) for i in commits]))[-1]
     header = "No  "+"Date".ljust(17)+"  "+\
@@ -41,7 +44,7 @@ def show_all():
     description="You can get commit hash. Please set index.", 
     argument_types={"index":int})
 def copy_hash(index):
-    commits = core.get_commits()
+    commits = adjust.get_commits()
     if index > len(commits)-1:
         print yellow("this index is over length limit.")
         return
@@ -53,12 +56,12 @@ def copy_hash(index):
     description="You can show commit difarence",
     argument_types={"index":int})
 def show_diff(index):
-    commits = core.get_commits()
+    commits = adjust.get_commits()
     if index > len(commits)-1:
         print yellow("this index is over length limit.")
         return
 
-    lines = core.shellrun("git show %s" % commits[index]).split("\n")
+    lines = git("show","%s" % commits[index]).split("\n")
     for line in lines:
         if line.startswith("+"):
             print red(line)
@@ -70,7 +73,7 @@ def show_diff(index):
             print line
 
 if __name__ == "__main__":
-    if core.check_exist_repo() == False:
+    if check_exist_repo() == False:
         print yellow("There is not git repository!")
         miniparser.kill(1)
 
--- util/adjust.py
+++ util/adjust.py
@@ -26,80 +26,73 @@ def get_commits():
 
     return commits
 
+def get_latest_commit():
+    """最新のコミットのCommitオブジェクトを返す"""
+    commits = []
+    commits_string = git("log", "-n", "1")
+
+    # analyzing
+    commitlines = commits_string.split("\n\n")
+    commits_string_list = []
+    while 0 < len(commitlines):
+        commits_string_list.append(
+                    "\n".join(commitlines[0:2]))
+        commitlines = commitlines[2:]
+
+    # parse
+    for commitline in commits_string_list:
+        commitdata = parse_commit(commitline)
+        commits.append(
+            Commit(**commitdata))
+
+    return commits[0]
 
 def parse_commit(commitstring):
     """シェルから取得したコミットログをパースして""" \
     """コミットのハッシュ値、コミットした者、日付・時間、コミットコメント及び""" \
     """マージした場合はsourceとdestinationを返す"""
-    commitdata = [i for i in commitstring.split("\n") if i != "    "]
-    if len(commitdata) == 5:
-        # Contain merge data
-        # -- sample -----------------------------------------
-        # 0 commit 9bb0df77e3f6727c873f8fee1e3f73f74b5ad633
-        # 1 Merge: b1f4e7e 943916f
-        # 2 Author: alice <takemehighermore@gmail.com>
-        # 3 Date:   Thu Nov 15 00:47:26 2012 +0900
-        # 4     Merge branch 'master' into rewrite
-        # ---------------------------------------------------
-        commithash   = commitdata[0].split(" ")[1]
-        merge_source = commitdata[1].split(" ")[1]
-        merge_dest   = commitdata[1].split(" ")[2]
-        author       = commitdata[2].split(" ")[1]
-        date         = dateparser.parse(
-                        commitdata[3].replace(
-                                "Date:   ",""))
-        comment      = commitdata[4].strip()
+    commitlines = commitstring.split("\n")
 
-        return {"commithash": commithash,
-                "merge_data": (merge_source, merge_dest),
-                "author"    : author,
-                "date"      : date,
-                "comment"   : comment}
+    # 1 row
+    commithash = commitlines[0].replace("commit ","")
 
-    elif len(commitdata) == 4:
-        # nomal
-        # -- sample -----------------------------------------
-        # 0 commit 943916fe88a2c247231f2f5f138e7d572e9248d5
-        # 1 Author: alice <takemehighermore@gmail.com>
-        # 2 Date:   Thu Nov 15 00:46:23 2012 +0900
-        # 3     added utilities
-        # ---------------------------------------------------
-        commithash   = commitdata[0].split(" ")[1]
-        author       = commitdata[1].split(" ")[1]
-        date         = dateparser.parse(
-                        commitdata[2].replace(
-                                "Date:   ",""))
-        comment      = commitdata[3].strip()
-
-        return {"commithash": commithash,
-                "merge_data": None,
-                "author"    : author,
-                "date"      : date,
-                "comment"   : comment}
+    # 2 row
+    merge_source = merge_dest = None
+    author = None
+    if "Merge" in commitlines[1]:
+        # parse merge data
+        merge_source = commitlines[1].split(" ")[1]
+        merge_dest = commitlines[1].split(" ")[2]
     else:
-        # abnomal
-        # -- sample -----------------------------------------
-        # 0 commit 311dda81133c69e9394a1c11c5eab7b1baf0477c
-        # 1 Merge: 5740af5 3691dad
-        # 2 Author: alice <genocidedragon@gmail.com>
-        # 3 Date:   Mon Nov 5 16:58:31 2012 -0800
-        # 4    Merge pull request #5 from yosida95/bugfix
-        # 5    pull request merge.
-        # ---------------------------------------------------
-        commithash   = commitdata[0].split(" ")[1]
-        merge_source = commitdata[1].split(" ")[1]
-        merge_dest   = commitdata[1].split(" ")[2]
-        author       = commitdata[2].split(" ")[1]
-        date         = dateparser.parse(
-                        commitdata[3].replace(
-                                "Date:   ",""))
-        comment      = ", ".join([i.strip() for i in commitdata[4:]])
+        # parse author
+        author = commitlines[1].replace("Author: ","")
 
-        return {"commithash": commithash,
-                "merge_data": (merge_source, merge_dest),
-                "author"    : author,
-                "date"      : date,
-                "comment"   : comment}
+    # 3 row
+    if "Author" in commitlines[2]:
+        # parse author
+        author = commitlines[2].replace("Author: ","")
+    else:
+        # parse date
+        date = dateparser.parse(commitlines[2].replace("Date:   ",""))
 
+    # 4 row
+    if "Date" in commitlines[3]:
+        # parse date
+        date = dateparser.parse(commitlines[3].replace("Date:   ",""))
+    else:
+        # parse comment
+        comment = " ".join([i.strip() for i in commitlines[3:]])
+
+    # 5 row
+    if "Merge" in commitlines[1]:
+        # comment -> under 4 row
+        comment = " ".join([i.strip() for i in commitlines[4:]])
+
+    return {"commithash": commithash,
+            "merge_data": (merge_source, merge_dest) if merge_source != None else None,
+            "author"    : author,
+            "date"      : date,
+            "comment"   : comment}
+        
 
 
