fe9f107478510a20bfcc5006fa7cbbeb25fc5f7a
tominaga <tominaga@dom10206a.local>
2013-09-20 15:18:14 +0900

    Created OriginalHelpFormatter class, and append attributes to argparse.HelpFormatter

--- git-todo2.py
+++ git-todo2.py
@@ -18,11 +18,74 @@ from datetime     import datetime
 from sys          import exit        as kill
 from gettext      import gettext     as _
 
-parser = argparse.ArgumentParser(description="You can manage to \
-                        What you want to do on your git repository.")
+class OriginalHelpFormatter(argparse.HelpFormatter):
+    def _format_action(self, action):
+        # determine the required width and the entry label
+        help_position = min(self._action_max_length + 2,
+                            self._max_help_position)
+        help_width = self._width - help_position
+        action_width = help_position - self._current_indent - 2
+        action_header = self._format_action_invocation(action)
+
+        # ho nelp; start on same line and add a final newline
+        if not action.help:
+            tup = self._current_indent, '', action_header
+            action_header = '%*s%s\n' % tup
+
+        # short action name; start on the same line and pad two spaces
+        elif len(action_header) <= action_width:
+            tup = self._current_indent, '', action_width, action_header
+            action_header = '%*s%-*s  ' % tup
+            indent_first = 0
+
+        # long action name; start on the next line
+        else:
+            tup = self._current_indent, '', action_header
+            action_header = '%*s%s\n' % tup
+            indent_first = help_position
+
+
+        # collect the pieces of the action help
+        parts = [action_header]
+
+        # if there was help for the action, add lines of help text
+        if action.help:
+            help_text = self._expand_help(action)
+            help_lines = self._split_lines(help_text, help_width)
+            parts.append('%*s%s\n' % (indent_first, '', help_lines[0]))
+            for line in help_lines[1:]:
+                parts.append('%*s%s\n' % (help_position, '', line))
+
+        # or add a newline if the description doesn't end with one
+        elif not action_header.endswith('\n'):
+            parts.append('\n')
+
+        # if there are any sub-actions, add their help as well
+        for subaction in self._iter_indented_subactions(action):
+            parts.append(self._format_action(subaction))
+
+            # Original Code
+            parts.append("\n")
+
+        # return a single string
+        return self._join_parts(parts)
+
+help_formatter = (lambda prog: 
+                    OriginalHelpFormatter(
+                        prog,
+                        indent_increment=4,
+                        max_help_position=27,
+                        width=int(core.terminal_width())
+                    )
+                 )
+
+parser = argparse.ArgumentParser(
+        prog="git todo",
+        formatter_class=help_formatter,
+        description="You can manage to What you want to do on your git repository.")
 
 subparsers = parser.add_subparsers(
-        title="git-todo commands",
+        title="git todo commands",
         dest="commands")
 
 cmd_init = subparsers.add_parser(
